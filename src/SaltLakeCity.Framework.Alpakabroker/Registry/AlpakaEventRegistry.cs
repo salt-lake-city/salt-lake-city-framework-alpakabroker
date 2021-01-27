3e532e6f4bfd46575cb94b93e9569fd903ba3236
NyghtX <jan.heinsohn.dev@gmail.com>
2021-01-27 12:26:08 +0100

    Abstractions

---
+++ src/SaltLakeCity.Framework.Alpakabroker/Registry/AlpakaEventRegistry.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using SaltLakeCity.Framework.Alpakabroker.Abstractions.Registry;
+
+namespace SaltLakeCity.Framework.Alpakabroker.Registry
+{
+    public class AlpakaEventRegistry
+    {
+        public static void Init(IEnumerable<AlpakaEventTypeComosite> alpakaEventTypeComosites)
+        {
+            
+        }
+    }
+}
